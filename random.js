efe2884f538eba1c3bb263342e8a2598aa0b4b6c
Alicia Miceli <aliciamiceli@Alicias-MacBook-Pro.local>
2016-09-01 20:10:16 -0500

    penultimate commit

---
+++ random.js
@@ -0,0 +1,27 @@
+var button = document.getElementById("button");
+var output = document.getElementById("output");
+
+button.addEventListener('click', randomFunction);
+
+
+function randomFunction() {
+var random = Math.ceil(Math.random(15) * 10);
+
+  if (random < 5 || random === 5) {
+//to ensure the numbers are random
+    console.log(random)
+    output.classList.add("arrow-right");
+    output.classList.remove("arrow-left");
+  } else if (random > 6 || random === 6) {
+//to ensure the numbers are random
+    console.log(random)
+    output.classList.add("arrow-left");
+    output.classList.remove("arrow-right");
+  }
+}
+
+
+
+
+
+
